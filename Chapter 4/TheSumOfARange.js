a84fd459e5906b9d237311af750d1ac9da72681c
kopaswdupas <lukasz.siedlaczek@gmail.com>
2014-12-22 11:41:52 +0100

    Update TheSumOfARange.js

--- Chapter 4/TheSumOfARange.js
+++ Chapter 4/TheSumOfARange.js
@@ -1,15 +1,22 @@
 function range(start, end, step) {
 	var sumArr = [start];
+	// po co Ci sprawdzanie czy jest jedynka?
 	if (step === 1 || step === undefined) {step = 1;}
-    for (var i = start; (start > end) ? (i > end) : (i < end) ; i+=step) {
-      sumArr.push(start += step);
+    	
+    	// srodkowy warunek sprawdzasz przy kazdej petli, nie lepiej sprawdzic to raz przed wejsciem w petle i potem operowac
+    	// na pojedynczym warunku?
+    	for (var i = start; (start > end) ? (i > end) : (i < end) ; i+=step) {
+      		sumArr.push(start += step);
 	};
+	
 	return sumArr;
 };
+
+// napisz jeszcze jeden raz funkcje sumujaca wartosci w array przy uzyciu natywnych metod arraya
 function sum(arr) {
 	var summ = 0;
 	for (var i = 0; i <= arr.length; i++) {
 		summ += i;
 	}
-  return summ;
+	return summ;
 };
